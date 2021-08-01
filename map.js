3c408be17dce8e447f7b767343b09875e37f22a7
JosManoel <josmanoelfreitas2012@gmail.com>
2021-08-01 18:24:37 -0300

    Test new UI - implement mobile layout

--- map.js
+++ map.js
@@ -1,32 +1,55 @@
+document.getElementById('submit').addEventListener('click', () => {
+    parent.window.location.href = "https://forms.gle/sQbwSUEczAYC68W8A";
+});
+
+document.getElementById('more').addEventListener('click', () => {
+    document.getElementById('links').remove('hide');
+});
+
 
 function initStreetViewer(panoramaData) {
- 
+    document.getElementById('zoom-in').addEventListener('click', function(e) {
+        panoramaViewer.setHfov(panoramaViewer.getHfov() - 10);
+    });
+    document.getElementById('zoom-out').addEventListener('click', function(e) {
+        panoramaViewer.setHfov(panoramaViewer.getHfov() + 10);
+    });
+    document.getElementById('fullscreen').addEventListener('click', function(e) {
+        panoramaViewer.toggleFullscreen();
+    });
+
+    var controls = document.getElementById('controls');
     let panorama = document.getElementById('panorama');
     panorama.classList.remove('hide');
+    controls.classList.remove('hide');
 
     const panoramaViewer = pannellum.viewer('panorama', panoramaData);
     
+    
+    
+
     let map = document.getElementById('map'); 
     map.classList.add('hide');
     
-    let btn = document.getElementById('goBack')
+    let btn = document.getElementById('goBack');
     btn.classList.remove('hide');
     btn.addEventListener('click', () => {
         panoramaViewer.destroy();
         panorama.classList.add('hide');
+        controls.classList.add('hide');
         map.classList.remove('hide');
-        btn.classList.add('hide');
-        setLocationName("");
+        setLocationName("Zelda Breath of The Wild Street View");
     })
 
 }
 
 
 
-async function mapSpotsToMap() {
+async function mapSpotsToMap(locationNameSearch) {
     const spotsData = await (await fetch('./spotsData.json')).json();
     for (let spot of spotsData) {
         let position = L.latLng([spot.y, spot.x]);
+
         L.marker(position).addTo(map)
         .bindPopup(spot.locationName)
         .on('click', () => {
@@ -45,8 +68,14 @@ function setLocationName(locationName){
 //creates map 
 var map = L.map('map', {
     crs: L.CRS.Simple,
-    minZoom: -0.5
+    minZoom: -0.5,
+    zoomControl: false
 });
+
+//add zoom control with your options
+L.control.zoom({
+    position:'topright'
+}).addTo(map);
 
 var bounds = [[0, 0], [1000, 2000]];
 var image = L.imageOverlay('map.jpg', bounds).addTo(map);
