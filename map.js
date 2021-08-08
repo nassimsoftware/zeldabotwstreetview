07eefdc2180411331a94a79b057953f53a42bdbd
JosManoel <josmanoelfreitas2012@gmail.com>
2021-08-08 15:47:22 -0300

    Add new layout page: Desktop and Mobile

--- map.js
+++ map.js
@@ -1,51 +1,65 @@
+var moreBtn = document.getElementById('more');
+var panel = document.getElementById('panel');
+
 document.getElementById('submit').addEventListener('click', () => {
     parent.window.location.href = "https://forms.gle/sQbwSUEczAYC68W8A";
 });
 
 document.getElementById('more').addEventListener('click', () => {
-    document.getElementById('links').remove('hide');
+    panel.classList.remove('hide');
+    moreBtn.classList.add('hide');
 });
 
+document.getElementById('hide').addEventListener('click', () => {        
+    panel.classList.add('hide');
+    moreBtn.classList.remove('hide');
+});
+
+function isMobile(){
+    return window.innerWidth <= 856 ? true : false;
+}
 
 function initStreetViewer(panoramaData) {
+    const panoramaViewer = pannellum.viewer('panorama', panoramaData);
+    let panorama = document.getElementById('panorama');
+    let controls = document.getElementById('controls');
+    let goBack = document.getElementById('goBack');
+    let map = document.getElementById('map'); 
+    
     document.getElementById('zoom-in').addEventListener('click', function(e) {
         panoramaViewer.setHfov(panoramaViewer.getHfov() - 10);
     });
+    
     document.getElementById('zoom-out').addEventListener('click', function(e) {
         panoramaViewer.setHfov(panoramaViewer.getHfov() + 10);
     });
+    
     document.getElementById('fullscreen').addEventListener('click', function(e) {
         panoramaViewer.toggleFullscreen();
     });
 
-    var controls = document.getElementById('controls');
-    let panorama = document.getElementById('panorama');
     panorama.classList.remove('hide');
-    controls.classList.remove('hide');
-
-    const panoramaViewer = pannellum.viewer('panorama', panoramaData);
-    
-    
-    
-
-    let map = document.getElementById('map'); 
+    goBack.classList.remove('hide');
     map.classList.add('hide');
     
-    let btn = document.getElementById('goBack');
-    btn.classList.remove('hide');
-    btn.addEventListener('click', () => {
+    // Custom panorama controls
+    if(!isMobile()){
+        controls.classList.remove('hide');
+    }
+
+    goBack.addEventListener('click', () => {
+
         panoramaViewer.destroy();
+
         panorama.classList.add('hide');
         controls.classList.add('hide');
         map.classList.remove('hide');
-        setLocationName("Zelda Breath of The Wild Street View");
-    })
 
+        setLocationName("Search by location");
+    })
 }
 
-
-
-async function mapSpotsToMap(locationNameSearch) {
+async function mapSpotsToMap() {
     const spotsData = await (await fetch('./spotsData.json')).json();
     for (let spot of spotsData) {
         let position = L.latLng([spot.y, spot.x]);
@@ -55,7 +69,6 @@ async function mapSpotsToMap(locationNameSearch) {
         .on('click', () => {
             initStreetViewer(spot.panoramaData);
             setLocationName(spot.locationName);
-
         })
     }
 }
@@ -72,13 +85,15 @@ var map = L.map('map', {
     zoomControl: false
 });
 
-//add zoom control with your options
-L.control.zoom({
-    position:'topright'
-}).addTo(map);
+//add custon zoom control 
+if(!isMobile()){
+    L.control.zoom({
+        position:'topright'
+    }).addTo(map);
+}
 
 var bounds = [[0, 0], [1000, 2000]];
 var image = L.imageOverlay('map.jpg', bounds).addTo(map);
 map.fitBounds(bounds);
-////
+
 mapSpotsToMap();
